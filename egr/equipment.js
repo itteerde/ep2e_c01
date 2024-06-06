099b488d0919efc38d7b3669e96d2827c3ab4137
Erik Itter <erik.itter@gmail.com>
2024-06-06 15:21:00 +0200

    change to gear Map

--- egr/equipment.js
+++ egr/equipment.js
@@ -1,30 +1,17 @@
-const gear = [
-	{ name: "Explorenaut", gp: 3 },
-	{ name: "Heavy Combat Armor", gp: 3, ware: "H" },
-	{ name: "Robomule", gp: 2 },
-	{ name: "DocBot", gp: 3 },
-	{ name: "Ecto", gp: 1 },
-	{ name: "Server", gp: 2, pg: 337 },
-	{ name: "Laser/Microwave Link (Micro)", gp: 1, pg: 336 },
-	{ name: "Weapon Mount", gp: 1, ware: "H", pg: 323 },
-	{ name: "Refractive Glazing", gp: 1, ware: "H", pg: 217 },
-	{ name: "Impact", gp: 1, ware: "H", pg: 217 },
-	{ name: "Battle Laser", gp: 3, ware: "H", pg: 208, properties: ["Long"] }
-];
+const gear = new Map();
 
-const equipment = [
-	{ name: "Explorenaut", gp: 3 },
-	{ name: "Heavy Combat Armor", gp: 3, ware: "H" },
-	{ name: "Robomule", gp: 2 },
-	{ name: "DocBot", gp: 3 },
-	{ name: "Ecto", gp: 1 },
-	{ name: "Server", gp: 2, pg: 337 },
-	{ name: "Laser/Microwave Link (Micro)", gp: 1, pg: 336 },
-	{ name: "Weapon Mount", gp: 1, ware: "H", pg: 323 },
-	{ name: "Refractive Glazing", gp: 1, ware: "H", pg: 217 },
-	{ name: "Impact", gp: 1, ware: "H", pg: 217 },
-	{ name: "Battle Laser", gp: 3, ware: "H", pg: 208, properties: ["Long"] }
-];
+gear.set("Explorenaut", { name: "Explorenaut", gp: 3, pg: 346 });
+gear.set("Heavy Combat Armor", { name: "Heavy Combat Armor", gp: 3, ware: ["H"], pg: 214 });
+gear.set("Robomule", { name: "Robomule", gp: 2 });
+gear.set("DocBot", { name: "DocBot", gp: 3, type: ["Robot"] });
+gear.set("Ecto", { name: "Ecto", gp: 1 });
+gear.set("Server", { name: "Server", gp: 2, pg: 337 });
+gear.set("Laser/Microwave Link (Micro)", { name: "Laser/Microwave Link (Micro)", gp: 1, pg: 336 });
+gear.set("Weapon Mount", { name: "Weapon Mount", gp: 1, ware: "H", pg: 323 });
+gear.set("Refractive Glazing", { name: "Refractive Glazing", gp: 1, ware: "H", pg: 217 });
+gear.set("Impact", { name: "Impact", gp: 1, ware: "H", pg: 217 });
+gear.set("Battle Laser", { name: "Battle Laser", gp: 3, ware: "H", pg: 208, properties: ["Long"] });
+
 
 const mission_gear = new Map();
 mission_gear.set("EGR_2.71828", {
@@ -32,9 +19,10 @@ mission_gear.set("EGR_2.71828", {
 	mp: 10,
 	gp: 24,
 	gear: [
-		gear.find(i => i.name === "DocBot"),
-		gear.find(i => i.name === "Server"),
-		gear.find(i => i.name === "Robomule")
+		structuredClone(gear.get("DocBot")),
+		gear.get("Server"),
+		gear.get("Robomule"),
+		structuredClone(gear.get("Heavy Combat Armor"))
 	],
 	morph: []
 });
