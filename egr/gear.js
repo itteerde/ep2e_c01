f59c3e299408d665743cabdc0e250542cd2b6d15
Erik Itter <erik.itter@gmail.com>
2024-07-31 20:13:32 +0200

    plans

--- egr/gear.js
+++ egr/gear.js
@@ -1,3 +1,8 @@
+class Blueprint {
+
+}
+
+
 await game.actors.getName("EGR_2.71828").setFlag("tablerules", "blueprints", [{
     name: "Superthermite Charge",
     page: 340,
@@ -13,3 +18,12 @@ await game.actors.getName("EGR_2.71828").setFlag("tablerules", "blueprints", [{
     ego_id: "EGR_2.71828",
     storage: "Mitre"
 }, ...(game.actors.getName("EGR_2.71828").getFlag("tablerules", "blueprints"))]);
+
+
+function software2gp(s) {
+    if (s === "minor") return 1;
+    if (s === "moderate") return 2;
+    if (s === "major") return 3;
+}
+game.actors.getName("EGR_2.71828").items.contents.filter(i => i.type === "software").map(function (s) { return { name: s.name, complexity: software2gp(s.system.complexity) } })
+game.actors.getName("EGR_2.71828").items.contents.filter(i => i.type === "software").map(function (s) { return { name: s.name, complexity: software2gp(s.system.complexity) } }).reduce((acc, v) => acc + v.complexity, -4)
---
+++ egr/planning.js
@@ -0,0 +1,8 @@
+const plans = [
+    {
+        id: self.crypto.randomUUID(),
+        tasks: [
+            { s: "SysRig.Exe", t: `mine Raw Materials ${{ Min: 0, Mod: 7, Maj: "rest" }}` }
+        ]
+    }
+];
